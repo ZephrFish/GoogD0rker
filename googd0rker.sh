47c43d784d61c3b9c6b2806c583149e70ea9203d
ZephrFish <ZephrFish@users.noreply.github.com>
2017-05-30 13:53:20 +0100

    Update googd0rker.sh

--- googd0rker.sh
+++ googd0rker.sh
@@ -14,30 +14,30 @@ sleep 5
 echo "Opening Browser with search terms per tab"
 
 # FIND LOGIN PAGES:
-open "https://www.google.com/search?q=site:$TARGET+username+OR+password+OR+login+OR+root+OR+admin" 2> /dev/null
+open "https://www.google.com/search?q=site:$TARGET+username+OR+password+OR+login+OR+root+OR+admin" --args --incognito 2> /dev/null
 # SEARCH FOR BACKDOORS:
-open "https://www.google.com/search?q=site:$TARGET+inurl:shell+OR+inurl:backdoor+OR+inurl:wso+OR+inurl:cmd+OR+shadow+OR+passwd+OR+boot.ini+OR+inurl:backdoor" 2> /dev/null
+open "https://www.google.com/search?q=site:$TARGET+inurl:shell+OR+inurl:backdoor+OR+inurl:wso+OR+inurl:cmd+OR+shadow+OR+passwd+OR+boot.ini+OR+inurl:backdoor" --args --incognito 2> /dev/null
 # FIND SETUP OR INSTALL FILES:
-open "https://www.google.com/search?q=site:$TARGET+inurl:readme+OR+inurl:license+OR+inurl:install+OR+inurl:setup+OR+inurl:config" 2> /dev/null
+open "https://www.google.com/search?q=site:$TARGET+inurl:readme+OR+inurl:license+OR+inurl:install+OR+inurl:setup+OR+inurl:config" --args --incognito 2> /dev/null
 # FIND WORDPRESS PLUGINS/UPLOADS/DOWNLOADS:
-open "https://www.google.com/search?q=site:$TARGET+inurl:wp-+OR+inurl:plugin+OR+inurl:upload+OR+inurl:download" 2> /dev/null
+open "https://www.google.com/search?q=site:$TARGET+inurl:wp-+OR+inurl:plugin+OR+inurl:upload+OR+inurl:download" --args --incognito 2> /dev/null
 # FIND OPEN REDIRECTS:
-open "https://www.google.com/search?q=site:$TARGET+inurl:redir+OR+inurl:url+OR+inurl:redirect+OR+inurl:return+OR+inurl:src=http+OR+inurl:r=http" 2> /dev/null
+open "https://www.google.com/search?q=site:$TARGET+inurl:redir+OR+inurl:url+OR+inurl:redirect+OR+inurl:return+OR+inurl:src=http+OR+inurl:r=http" --args --incognito 2> /dev/null
 # FIND FILES BY EXTENSION:
-open "https://www.google.com/search?q=site:$TARGET+ext:cgi+OR+ext:php+OR+ext:asp+OR+ext:aspx+OR+ext:jsp+OR+ext:jspx+OR+ext:swf+OR+ext:fla+OR+ext:xml" 2> /dev/null
+open "https://www.google.com/search?q=site:$TARGET+ext:cgi+OR+ext:php+OR+ext:asp+OR+ext:aspx+OR+ext:jsp+OR+ext:jspx+OR+ext:swf+OR+ext:fla+OR+ext:xml" --args --incognito 2> /dev/null
 # FIND DOCUMENTS BY EXTENSION:
-open "https://www.google.com/search?q=site:$TARGET+ext:doc+OR+ext:docx+OR+ext:csv+OR+ext:pdf+OR+ext:txt+OR+ext:log+OR+ext:bak" 2> /dev/null
+open "https://www.google.com/search?q=site:$TARGET+ext:doc+OR+ext:docx+OR+ext:csv+OR+ext:pdf+OR+ext:txt+OR+ext:log+OR+ext:bak" --args --incognito 2> /dev/null
 # FIND APACHE STRUTS RCE's:
-open "https://www.google.com/search?q=site:$TARGET+ext:action+OR+ext:struts+OR+ext:do" 2> /dev/null
+open "https://www.google.com/search?q=site:$TARGET+ext:action+OR+ext:struts+OR+ext:do" --args --incognito 2> /dev/null
 # FIND PASTEBIN POSTS FOR DOMAIN:
-open "https://www.google.com/search?q=site:pastebin.com+$TARGET" 2> /dev/null
+open "https://www.google.com/search?q=site:pastebin.com+$TARGET" --args --incognito 2> /dev/null
 # FIND EMPLOYEES ON LINKEDIN:
-open "https://www.google.com/search?q=site:linkedin.com+employees+$TARGET" 2> /dev/null
+open "https://www.google.com/search?q=site:linkedin.com+employees+$TARGET" --args --incognito 2> /dev/null
 # FIND Subdomains
-open "https://www.google.com/search?q=site:*.$TARGET" 2> /dev/null
+open "https://www.google.com/search?q=site:*.$TARGET" --args --incognito 2> /dev/null
 # FIND Sub-subdomains
-open "https://www.google.com/search?q=site:*.*.$TARGET" 2> /dev/null
+open "https://www.google.com/search?q=site:*.*.$TARGET" --args --incognito 2> /dev/null
 # FIND PHPINFO Files
-open "https://www.google.com/search?q=inurl:'/phpinfo.php'+$TARGET" 2> /dev/null
+open "https://www.google.com/search?q=inurl:'/phpinfo.php'+$TARGET" --args --incognito 2> /dev/null
 # FIND .htaccess & sensitive fiels
-open "https://www.google.com/search?q=inurl:'/phpinfo.php'+OR+inurl:'.htaccess'+OR+inurl:'/.git'+$TARGET+-github" 2> /dev/null
+open "https://www.google.com/search?q=inurl:'/phpinfo.php'+OR+inurl:'.htaccess'+OR+inurl:'/.git'+$TARGET+-github" --args --incognito 2> /dev/null
