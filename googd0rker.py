2ce3eeeff73bebf03190a3454533d62b5c2d1223
ZephrFish <ZephrFish@users.noreply.github.com>
2017-08-28 12:04:42 +0100

    added sleep of random INT between requests: sleep(randint(10,100))

--- googd0rker.py
+++ googd0rker.py
@@ -1,11 +1,13 @@
-# GoogD0rker 0.2 - TXT Edition
-# Written by https://github.com/txt3rob/bugbountydork originally
+# GoogD0rker 0.3 
 # Merging changes into the main repo
 # Follow RandomRobbie on the twitters, https://twitter.com/Random_Robbie
+# Added in Sleep inbetween requests
 
 from google import search
 import os
 import argparse
+from random import randint
+from time import sleep
 
 
 # Add in argument options
@@ -45,29 +47,43 @@ def google_it (site,dork,filename):
 
 print ("Finding Login Pages for "+site+"\n")
 google_it (site,"site:"+site+" inurl:wp- OR inurl:plugin OR inurl:upload OR inurl:download","loginpage.txt")
+sleep(randint(10,100))
 print ("Finding Backdoors for "+site+"\n")
 google_it (site,"site:"+site+" inurl:shell OR inurl:backdoor OR inurl:wso OR inurl:cmd OR shadow OR passwd OR boot.ini OR inurl:backdoor","backdoor.txt")
+sleep(randint(10,100))
 print ("Finding Install / Setup files for "+site+"\n")
 google_it (site,"site:"+site+" inurl:readme OR inurl:license OR inurl:install OR inurl:setup OR inurl:config","setup_files.txt")
+sleep(randint(10,100))
 print ("Finding WORDPRESS PLUGINS/UPLOADS/DOWNLOADS for "+site+"\n")
 google_it (site,"site:"+site+" inurl:wp- OR inurl:plugin OR inurl:upload OR inurl:download","wordpress.txt")
+sleep(randint(10,100))
 print ("Finding OPEN REDIRECTS for "+site+"\n")
 google_it (site,"site:"+site+" inurl:redir OR inurl:url OR inurl:redirect OR inurl:return OR inurl:src=http OR inurl:r=http" ,"openredirect.txt")
+sleep(randint(10,100))
 print ("Finding FILES BY EXTENSION for "+site+"\n")
 google_it (site,"site:"+site+" ext:cgi OR ext:php OR ext:asp OR ext:aspx OR ext:jsp OR ext:jspx OR ext:swf OR ext:fla OR ext:xml" ,"extentions.txt")
+sleep(randint(10,100))
 print ("Finding FIND DOCUMENTS BY EXTENSION for "+site+"\n")
 google_it (site,"site:"+site+" ext:doc OR ext:docx OR ext:csv OR ext:pdf OR ext:txt OR ext:log OR ext:bak" ,"documents.txt")
+sleep(randint(10,100))
 print ("Finding FIND APACHE STRUTS RCE for "+site+"\n")
 google_it (site,"site:"+site+" ext:action OR ext:struts OR ext:do" ,"struts.txt")
+sleep(randint(10,100))
 print ("Finding PASTEBIN POSTS FOR DOMAIN for "+site+"\n")
 google_it (site,"site:pastebin.com "+site+"" ,"pastebin.txt")
+sleep(randint(10,100))
 print ("Finding EMPLOYEES ON LINKEDIN for "+site+"\n")
 google_it (site,"site:linkedin.com employees "+site+"" ,"linkedin.txt")
+sleep(randint(10,100))
 print ("Finding Subdomains for "+site+"\n")
 google_it (site,"site:*."+site+"" ,"subdomains.txt")
+sleep(randint(10,100))
 print ("Finding Sub-subdomains for "+site+"\n")
 google_it (site,"site:*.*."+site+"" ,"sub-subdomains.txt")
+sleep(randint(10,100))
 print ("Finding PHPINFO Files for "+site+"\n")
 google_it (site,"inurl:'/phpinfo.php' "+site+"" ,"phpinfo.txt")
+sleep(randint(10,100))
 print ("Finding .htaccess & sensitive fiels for "+site+"\n")
 google_it (site,"inurl:'/phpinfo.php' OR inurl:'.htaccess' OR inurl:'/.git' "+site+" -github" ,"sensitive.txt")
+time.sleep(5)
